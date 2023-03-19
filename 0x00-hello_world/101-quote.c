d1b5bc908310a4db858e02d76e36b664eac27543
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-03-19 15:28:57 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * Description - A c program that prints exactly
  * Return: 1 always
- */	
-int main(void)	
+ */
+int main(void)
 {
 	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-}	
+}
