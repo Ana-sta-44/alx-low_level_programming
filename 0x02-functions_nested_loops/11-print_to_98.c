6ba3fe66b05d0bba5c8bc4d2a676760da4e1898f
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-03-21 17:11:36 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (n >= 0 && n <= 98);
+	for (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
@@ -23,7 +23,7 @@ void print_to_98(int n)
 		printf("\n");
 
 	}
-	else if (n < 0)
+	else if (n < 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
