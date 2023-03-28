b34c7509855738a0fe35d02b98002f358cc9a6a1
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-03-28 15:30:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: 1 always
+ *main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: always 1.
  */
+
+
+
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
+	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
