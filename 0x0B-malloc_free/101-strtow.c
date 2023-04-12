141acec0f7ab1a2fb457dac191e4050e76eb397e
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-04-12 08:58:58 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void util(char **, char *);
-void create_word(char **, char *, int, int,int);
+void create_word(char **, char *, int, int, int);
 
 /**
  * strtow - spliits a string into words.
@@ -18,7 +18,7 @@ char **strtow(char *str)
 	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
 
-	i = flag = len =0;
+	i = flag = len = 0;
 	while (str[i])
 	{
 		if (flag == 0 && str[i] != ' ')
@@ -50,7 +50,7 @@ char **strtow(char *str)
  */
 void util(char **words, char *str)
 {
-	int i, j, star, flag;
+	int i, j, start, flag;
 
 	i = j = flag = 0;
 	while (str[i])
@@ -73,4 +73,24 @@ void util(char **words, char *str)
 
 	if (flag == 1)
 		create_word(words, str, start, i, j);
+}
+
+/**
+ * create_word - creates a word and insert it into the array
+ * words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ */
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
+
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
+		words[index][j] = str[start];
+	words[index][j] = '\0';
 }
