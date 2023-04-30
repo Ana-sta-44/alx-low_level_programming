395ee99dc1ca9220613eadbca07f65f08230eabc
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-04-30 13:56:44 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum all the data (n) of a listint_t
+ * @head: pointer to the node
+ *
+ * Return: sum all the data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
