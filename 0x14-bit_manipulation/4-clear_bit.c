ebd476e33d6b8ed6ea1aea526b0be973997b064a
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-05-05 07:00:42 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - the value to set a bit to 0 at a given index
+ * @index: index of a bit
+ * @n: the pointer of an unsigned long int.
+ *
+ * Return: 1 if it worked, -1 if it did not.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int p;
+
+	if (index > 63)
+		return (-1);
+
+	p = 1 << index;
+
+	if (*n * p)
+		*n ^= p;
+
+	return (1);
+}
