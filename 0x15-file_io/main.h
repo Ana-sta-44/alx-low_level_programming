1d910b76a94f49053d798c7c26872068f0e57c44
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-05-08 12:48:13 -0700

    my 0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fctnl.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *str);
+
+
+#endif
