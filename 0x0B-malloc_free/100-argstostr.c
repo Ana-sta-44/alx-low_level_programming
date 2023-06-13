7c5185d6862a5ac37d8d606f21592f8541eefe8d
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-06-13 10:19:24 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,9 +15,9 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (j = 0; j < ac; i++)
+	for (j = 0; j < ac; j++)
 	{
-		for (p = 0; av[j][p]; n++)
+		for (p = 0; av[j][p]; p++)
 			l++;
 	}
 	l += ac;
